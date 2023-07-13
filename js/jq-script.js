c6599f39d2b6be1ba41f33ce23f3917f15b1f5c3
Mark Saville <mark.saville@outlook.com>
2023-07-13 16:18:37 +0200

    create jquery script for to-do list functionality

---
+++ js/jq-script.js
@@ -0,0 +1,41 @@
+function newItem(){
+
+    //jquery
+    //1. Adding a new item to the list of items: 
+
+        let li = $('<li></li>');
+        let inputValue = $('#input').val();
+        li.append(inputValue);
+
+        /* let li = document.createElement("li");
+        let inputValue = document.getElementById("input").value;
+        let text = document.createTextNode(inputValue);
+        li.appendChild(text); */
+        
+  
+        if (inputValue === '') {
+            alert("You must write something!");
+        } else {
+            $('#list').append(li);
+        }
+        
+        //2. Crossing out an item from the list of items:
+        li.on("click", function() {
+            li.addClass("strike");
+          });
+        
+        //3(i). Adding the delete button "X": 
+        let crossOutButton = $('<crossOutButton></crossOutButton>');
+            crossOutButton.append(document.createTextNode('X'));
+            li.append(crossOutButton);
+        
+            $('crossOutButton').on("click", deleteListItem);
+        //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+        function deleteListItem(){
+                li.addClass("delete")
+            }
+        // 4. Reordering the items: 
+        $('#list').sortable();
+        
+    
+    }
